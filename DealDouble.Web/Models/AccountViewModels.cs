b0a88b223f112c94a464bc464261a854f64d33e3
Sajjad Gul <sajjadarifgul@gmail.com>
2018-12-12 21:05:54 +0500

    Session 12 : Identity Framework

--- DealDouble.Web/Models/AccountViewModels.cs
+++ DealDouble.Web/Models/AccountViewModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using DealDouble.Web.ViewModels;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace DealDouble.Web.Models
@@ -46,7 +47,7 @@ namespace DealDouble.Web.Models
         public string Email { get; set; }
     }
 
-    public class LoginViewModel
+    public class LoginViewModel : PageViewModel
     {
         [Required]
         [Display(Name = "Email")]
