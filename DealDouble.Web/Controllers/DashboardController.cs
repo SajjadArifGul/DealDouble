587024d3aa2ee873a35e929c4b7a4958d78df416
Sajjad Gul <sajjadarifgul@gmail.com>
2019-02-21 23:01:58 -0800

    Revert "User Listing done"
    This reverts commit fc479d794815122dcf4e46de5d687c9a39bf8675.

--- DealDouble.Web/Controllers/DashboardController.cs
+++ DealDouble.Web/Controllers/DashboardController.cs
@@ -6,50 +6,11 @@ using System.Web;
 using System.Web.Mvc;
 using DealDouble.Web.Code.Enums;
 using DealDouble.Services;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.Owin;
-using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace DealDouble.Web.Controllers
 {
     public class DashboardController : Controller
     {
-        private DealDoubleRolesManager _rolesManager;
-        private DealDoubleUserManager _userManager;
-
-        public DashboardController()
-        {
-        }
-
-        public DashboardController(DealDoubleUserManager userManager, DealDoubleRolesManager rolesManager)
-        {
-            UserManager = userManager;
-            RolesManager = rolesManager;
-        }
-
-        public DealDoubleRolesManager RolesManager
-        {
-            get
-            {
-                return _rolesManager ?? HttpContext.GetOwinContext().Get<DealDoubleRolesManager>();
-            }
-            private set
-            {
-                _rolesManager = value;
-            }
-        }
-        public DealDoubleUserManager UserManager
-        {
-            get
-            {
-                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<DealDoubleUserManager>();
-            }
-            private set
-            {
-                _userManager = value;
-            }
-        }
-
         DashboardService service = new DashboardService();
 
         public ActionResult Index()
@@ -63,37 +24,6 @@ namespace DealDouble.Web.Controllers
             model.BidsCount = service.GetBidsCount();
 
             return View(model);
-        }
-
-        public ActionResult Users(string userSearch, int? pageNo)
-        {
-            pageNo = pageNo ?? 1;
-            var pageSize = 10;
-
-            UsersViewModel model = new UsersViewModel();
-            model.Page = Pages.Users;
-            model.UserSearch = userSearch;
-
-            var allUsers = UserManager.Users;
-
-            if (!string.IsNullOrEmpty(userSearch))
-            {
-                allUsers = allUsers.Where(x => x.UserName.ToLower().Contains(userSearch.ToLower()));
-            }
-
-            var skipCount = (pageNo.Value - 1) * pageSize;
-
-            model.Users = allUsers.OrderBy(x => x.UserName).Skip(skipCount).Take(pageSize).ToList();
-
-            model.Roles = RolesManager.Roles.ToList();
-
-            model.Pager = new Pager(allUsers.Count(), pageNo, pageSize);
-
-            if(Request.IsAjaxRequest())
-            {
-                return PartialView("_UserListing", model);
-            }
-            else return View(model);
         }
     }
 }
--- DealDouble.Web/ViewModels/DashboardViewModels.cs
+++ DealDouble.Web/ViewModels/DashboardViewModels.cs
@@ -1,16 +1,7 @@
-﻿using DealDouble.Entities;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
-using System.Globalization;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.Owin;
-using Microsoft.Owin.Security;
-using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace DealDouble.Web.ViewModels
 {
@@ -19,14 +10,5 @@ namespace DealDouble.Web.ViewModels
         public int UserCount { get; set; }
         public int AuctionsCount { get; set; }
         public int BidsCount { get; set; }
-    }
-
-    public class UsersViewModel : PageViewModel
-    {
-        public List<DealDoubleUser> Users { get; set; }
-        public List<IdentityRole> Roles { get; set; }
-        public string UserSearch { get; internal set; }
-
-        public Pager Pager { get; set; }
     }
 }
