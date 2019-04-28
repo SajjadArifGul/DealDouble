48f4859dfe38f58727168f7fdff2f9b66aa477d5
Sajjad Gul <sajjadarifgul@gmail.com>
2019-04-28 06:34:20 +0500

    User Avatar

--- DealDouble.Web/Code/Helpers/PictureHelper.cs
+++ DealDouble.Web/Code/Helpers/PictureHelper.cs
@@ -16,9 +16,9 @@ namespace DealDouble.Web.Code.Helpers
             return Picture(htmlHelper, picURL, classes, style, alt);
         }
 
-        public static MvcHtmlString Picture(this HtmlHelper htmlHelper, string pictureURL, string classes = "", string style = "", string alt = "")
+        public static MvcHtmlString Picture(this HtmlHelper htmlHelper, string pictureURL, string classes = "", string style = "", string alt = "", string defaultPic = "site/default-picture.png")
         {
-            pictureURL = string.IsNullOrEmpty(pictureURL) ? "site/default-picture.png" : pictureURL;
+            pictureURL = string.IsNullOrEmpty(pictureURL) ? defaultPic : pictureURL;
 
             var image = new TagBuilder("img");
             image.AddCssClass(classes);
@@ -27,6 +27,18 @@ namespace DealDouble.Web.Code.Helpers
             image.MergeAttribute("alt", alt);
 
             return MvcHtmlString.Create(image.ToString());
+        }
+
+        public static MvcHtmlString UserAvatar(this HtmlHelper htmlHelper, Picture picture, string classes = "", string style = "", string alt = "")
+        {
+            var picURL = picture != null ? picture.URL : "";
+
+            return Picture(htmlHelper, picURL, classes, style, alt, "site/user-default-avatar.png");
+        }
+
+        public static MvcHtmlString UserAvatar(this HtmlHelper htmlHelper, string pictureURL, string classes = "", string style = "", string alt = "")
+        {
+            return Picture(htmlHelper, pictureURL, classes, style, alt, "site/user-default-avatar.png");
         }
     }
 }
